41650764a8eb4382ed456f23b467c3ed86bf745c
fcurrk <48197714+fcurrk@users.noreply.github.com>
2025-10-23 11:53:01 +0800

    Update

---
+++ release.sh
@@ -0,0 +1,16 @@
+# replace version
+cd OpenList-Frontend
+# version=$(git describe --abbrev=0 --tags)
+# sed -i -e "s/\"version\": \"0.0.0\"/\"version\": \"$version\"/g" package.json
+# cat package.json
+
+# build
+pnpm install
+# pnpm i18n:release
+pnpm build
+cp -r dist ../
+cd ..
+
+mkdir compress
+tar -czvf compress/dist.tar.gz dist/*
+zip -r compress/dist.zip dist/*
