f16e40d204d7facad4726ad887ab0bce9cf64fee
fcurrk <48197714+fcurrk@users.noreply.github.com>
2026-09-08 00:20:15 +0800

    Update 4.2.6260908

--- src/lang/zh-CN/entry.ts
+++ src/lang/zh-CN/entry.ts
@@ -4,9 +4,11 @@ import global from "./global.json"
 import home from "./home.json"
 import index from "./index.json"
 import indexes from "./indexes.json"
+import init from "./init.json"
 import login from "./login.json"
 import manage from "./manage.json"
 import metas from "./metas.json"
+import plugins from "./plugins.json"
 import settings_other from "./settings_other.json"
 import settings from "./settings.json"
 import shares from "./shares.json"
@@ -21,9 +23,11 @@ export const dict = {
   home,
   index,
   indexes,
+  init,
   login,
   manage,
   metas,
+  plugins,
   settings_other,
   settings,
   shares,
